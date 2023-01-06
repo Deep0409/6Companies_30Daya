cf84fbbe39325c7facd1b2923bd5d26b9dd16416
Deepanshu Pariyani <105860870+Deep0409@users.noreply.github.com>
2023-01-06 17:36:40 +0530

    Update Shortest_Unsorted_Continuous_Subarray.cpp

--- Microsoft/Shortest_Unsorted_Continuous_Subarray.cpp
+++ Microsoft/Shortest_Unsorted_Continuous_Subarray.cpp
@@ -4,20 +4,20 @@ public:
         
         int n = nums.size(),l = 0,r = n - 1;
         
-        while(l<n-1 && nums[l]<=nums[l+1]) l++;
-        while(r>0   && nums[r]>=nums[r-1]) r--;
+        while(l<n-1 && nums[l]<=nums[l+1]) l++; //removing the sorted part of starting
+        while(r>0   && nums[r]>=nums[r-1]) r--;//removing the sorted part of last
         
         if(l>= r)return 0;
         
         int mini = nums[l],maxi= nums[r];
         
-        for(int i = l; i <= r; i++)
+        for(int i = l; i <= r; i++)  //finding smaller and larger element 
         {
             maxi =maxi>nums[i]?maxi:nums[i];
             mini =mini>nums[i]?nums[i]:mini; 
         }
         
-        while(r<n &&nums[r]<maxi)r++;   
+        while(r<n &&nums[r]<maxi)r++;   //finding position of smaller and larger element
         while(l>=0&&nums[l]>mini)l--;
         
         return r - l - 1;
