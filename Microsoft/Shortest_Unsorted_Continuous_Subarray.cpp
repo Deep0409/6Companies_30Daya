779aeeedfe094670b444c3a8445164b1c3d19bb5
Deepanshu Pariyani <pariyanideepanshu@gmail.com>
2023-01-06 17:35:07 +0530

    Microsoft_Challenge

---
+++ Microsoft/Shortest_Unsorted_Continuous_Subarray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        
+        int n = nums.size(),l = 0,r = n - 1;
+        
+        while(l<n-1 && nums[l]<=nums[l+1]) l++;
+        while(r>0   && nums[r]>=nums[r-1]) r--;
+        
+        if(l>= r)return 0;
+        
+        int mini = nums[l],maxi= nums[r];
+        
+        for(int i = l; i <= r; i++)
+        {
+            maxi =maxi>nums[i]?maxi:nums[i];
+            mini =mini>nums[i]?nums[i]:mini; 
+        }
+        
+        while(r<n &&nums[r]<maxi)r++;   
+        while(l>=0&&nums[l]>mini)l--;
+        
+        return r - l - 1;
+        
+    }
+};
